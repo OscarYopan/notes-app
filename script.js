4e47cabe43ebb95bf5a5628b392a312848fc30cc
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-30 23:14:01 -0300

    --

--- script.js
+++ script.js
@@ -7,30 +7,38 @@ function addNewNote(text = '') {
   note.classList.add('note')
 
   note.innerHTML = `
-    <div class="tools">
-      <button class="edit"  title="Edit">
-        <i class="fa-solid fa-pen-to-square"></i>        
-      </button>
-
-      <button class="delete" title="Delete">
-        <i class="fa-solid fa-trash-can"></i>
-      </button>
-    </div>
-
-    <div class="main" ${text ? '' : 'hidden'}></div>
-    <textarea class="${text ? 'hidden' : ''}"></textarea>
+  <div class="tools">
+      <button class="edit"><i class="fas fa-edit"></i></button>
+      <button class="delete"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>
   `
-  document.body.appendChild(note)
 
-  const editBtn = document.querySelector('.edit')
-  const deleteBtn = document.querySelector('.delete')
-  const main = document.querySelector('.main')
-  const textArea = document.querySelector('textarea')
+  const editBtn = note.querySelector('.edit')
+  const deleteBtn = note.querySelector('.delete')
+  const main = note.querySelector('.main')
+  const textArea = note.querySelector('textarea')
+
+  textArea.value = text
+  main.innerHTML = marked(text)
 
   deleteBtn.addEventListener('click', () => {
-    note.remove()
+      note.remove()
+
+      updateLS()
   })
 
+  editBtn.addEventListener('click', () => {
+      main.classList.toggle('hidden')
+      textArea.classList.toggle('hidden')
+  })
 
-  
+  textArea.addEventListener('input', (e) => {
+      const { value } = e.target
+
+      main.innerHTML = marked(value)
+  })
+
+  document.body.appendChild(note)
 }
